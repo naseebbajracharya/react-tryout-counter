a305dd833cf1026c073903c1698b5a735fb0d72d
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2021-01-10 20:49:17 +0545

    counter component added

---
+++ src/components/counter.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+    state = {
+        count: 0
+    };
+
+    render() { 
+        React.createElement('h1');
+        return (
+            <React.Fragment>
+            <span className="badge badge-warning m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+            </React.Fragment>
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+}
+ 
+export default Counter;
